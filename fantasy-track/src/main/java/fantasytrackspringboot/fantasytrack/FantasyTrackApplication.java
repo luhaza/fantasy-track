633e2c114bb6480d62178e999b720bca7b198a43
Rick Yanashita <ry5@williams.edu>
2024-01-23 20:59:19 -0500

    make spring boot structure

---
+++ fantasy-track/src/main/java/fantasytrackspringboot/fantasytrack/FantasyTrackApplication.java
@@ -0,0 +1,13 @@
+package fantasytrackspringboot.fantasytrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyTrackApplication.class, args);
+	}
+
+}
---
+++ fantasy-track/src/test/java/fantasytrackspringboot/fantasytrack/FantasyTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package fantasytrackspringboot.fantasytrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
