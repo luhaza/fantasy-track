74d7c8b7ccc849b77032490bbe54868d0be73f1d
Rick Yanashita <ry5@williams.edu>
2024-01-29 18:24:15 -0500

    copy reference to start actual project

---
+++ fantasy-track/backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+    'mongodb+srv://root:kzXsqLS11djsMZQ5@books-store-mern.0fwhnam.mongodb.net/books-collection?retryWrites=true&w=majority'
---
+++ fantasy-track/backend/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS policy
+app.use(cors());
+
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Welcome to MERN stack tutorial!')
+});
+
+app.use('/books', booksRoute);
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to the database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`)
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
