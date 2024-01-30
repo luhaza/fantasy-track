8010f2fae0d80e1914065e9c6698689b916848b7
Rick Yanashita <ry5@williams.edu>
2024-01-29 21:00:30 -0500

    begin the UI and components with tailwind

--- fantasy-track/frontend/src/App.jsx
+++ fantasy-track/frontend/src/App.jsx
@@ -5,12 +5,15 @@ import CreateBook from './pages/CreateBook'
 import ShowBook from './pages/ShowBook'
 import EditBook from './pages/EditBook'
 import DeleteBook from './pages/DeleteBook'
+import CurrentCompetitions from './pages/CurrentCompetitions'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/currentcomp' element={<CurrentCompetitions />} />
       <Route path='/books/details/:id' element={<ShowBook />} />
       <Route path='/books/edit/:id' element={<EditBook />} />
       <Route path='/books/delete/:id' element={<DeleteBook />} />
--- fantasy-track/frontend/src/pages/Home.jsx
+++ fantasy-track/frontend/src/pages/Home.jsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import Spinner from '../components/Spinner'
 import { Link } from 'react-router-dom'
-import { AiOutlineEdit } from 'react-icons/ai'
 import { BsInfoCircle } from 'react-icons/bs'
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
 
 const Home = () => {
   const [books, setBooks] = useState([])
@@ -25,14 +22,60 @@ const Home = () => {
   }, []);
 
   return (
-    <div className='p-4'>
-      <div className='flex justify-between items-center'>
+    <div class="">
+
+      {/* navbar */}
+      <nav class="bg-white dark:bg-gray-900 fixed z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600 w-full">
+        {/* max-w-screen-xl */}
+        <div class="flex flex-wrap items-center justify-between pr-8 pl-8 pt-4 pb-4">
+        <a href="https://google.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+            {/* <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo"/> */}
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Fantasy Track</span>
+        </a>
+        <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign Up</button>
+            <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+              <span class="sr-only">Open main menu</span>
+              <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+              </svg>
+          </button>
+        </div>
+        <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky navbar-dropdown">
+          <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Current Competitions
+              <Link to={`/currentcomp`}> </Link>
+              </a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Leaderboards</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+            </li>
+          </ul>
+        </div>
+        </div>
+      </nav>
+
+      <div class="min-h-screen flex items-center justify-center bg-blue-500">
+        <p class="text-white">This div stretches the width and height of the page.</p>
+      </div>
+
+
+
+
+      {/* <div className='flex justify-between items-center'>
         <h1 className='text-3xl my-8'>Books List</h1>
         <Link to='/books/create'>
           <MdOutlineAddBox className='text-sky-800 text-4xl' />
         </Link>
-      </div>
-      {loading ? (
+      </div> */}
+      {/* {loading ? (
         <Spinner />
       ) : (
         <table className='w-full border-separate border-spacing-2'>
@@ -77,7 +120,7 @@ const Home = () => {
             ))}
           </tbody>
         </table>
-      )}
+      )} */}
     </div>
   )
 }
