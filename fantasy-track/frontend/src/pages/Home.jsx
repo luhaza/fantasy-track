c9ed075f4a18cd66313fed1529b08ac6f6b6bef5
Rick Yanashita <ryanashita15@gmail.com>
2024-02-01 17:59:52 -0500

    ui changes to Home

--- fantasy-track/frontend/src/pages/Home.jsx
+++ fantasy-track/frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { BsInfoCircle } from 'react-icons/bs'
 import heroImage from '../images/ncaa-d1-womens-hero.jpeg';
+import '../style.css';
 
 const Home = () => {
   const [books, setBooks] = useState([])
@@ -80,23 +81,54 @@ const Home = () => {
                               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                           </svg>
                       </a>
-                      <a href="#" class="inline-flex justify-center items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-white hover:text-blue-500">
+                      <a href="#moreinfo" class="inline-flex justify-center items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-white hover:text-blue-500">
                           Learn more
                       </a>  
                   </div>
               </div>
           </section>
         </div>
-        
-        {/* <div class="w-1/2 p-16">
-          <div class="bg-white rounded-md shadow-md shadow-grey p-4 h-screen/3 overflow-hidden">
-            <h1 class="text-4xl font-bold text-blue-500"> Join the Fantasy Track Community!</h1>
-            <h2 class="text-2xl font-semibold text-blue-500"> Create your track & field superteam. Play in meet-day drafts. Earn bragging rights</h2>
-            <p class="text-xl font-regular text-black"> Lorem ipsum dolor sit amet, eum veniam omnium eligendi ad, vel liber verterem urbanitas in. Nam at volumus epicuri urbanitas, nec at reque meliore quaerendum, duo no ferri option nostrum. Ut eam propriae repudiare persecuti, te sit nominati consulatu, eam salutandi constituto eu. Ne magna utamur pri, iudico mediocritatem ne ius, vel feugiat deserunt imperdiet id.Habeo illum habemus duo cu, vis ne putent feugait, unum vituperatoribus id sea. Natum intellegam ex quo. Ne nemore epicuri mea, melius torquatos eos ut. Choro iriure iudicabit eam id, pro dicat ridens maiestatis id. Ad cum docendi philosophia.</p>
-           
-          </div>
-        </div> */}
-        
+      </div>
+
+      <div class="min-h-screen bg-white flex items-center bg-transparent">
+        <div class="w-screen">
+          <section class="bg-white" id="moreinfo">
+              <div class="pb-14 px-4 pt-20 mx-auto max-w-screen-xl">
+                  <div class="grid md:grid-cols-2 gap-8">
+                      <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+                          <a href="#" class="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 mb-2">
+                              <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+                                  <path d="M17 11h-2.722L8 17.278a5.512 5.512 0 0 1-.9.722H17a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1ZM6 0H1a1 1 0 0 0-1 1v13.5a3.5 3.5 0 1 0 7 0V1a1 1 0 0 0-1-1ZM3.5 15.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM16.132 4.9 12.6 1.368a1 1 0 0 0-1.414 0L9 3.55v9.9l7.132-7.132a1 1 0 0 0 0-1.418Z"/>
+                              </svg>
+                              Tutorial
+                          </a>
+                          <h2 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">How to play fantasy track & field</h2>
+                          <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Fantasy sports are synonymous with complex rules & a lot of data. Learn how to navigate the fantasy track website and play in a meet-day draft.</p>
+                          <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Read more
+                              <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                          </svg>
+                          </a>
+                      </div>
+                      <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+                          <a href="#" class="bg-purple-100 text-purple-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-purple-400 mb-2">
+                              <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 4 1 8l4 4m10-8 4 4-4 4M11 1 9 15"/>
+                              </svg>
+                              Development Journey
+                          </a>
+                          <h2 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">This wasn't developed overnight</h2>
+                          <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Our dev team has put in countless hours developing a competitive and nice-looking track & field draft. Read what we've accomplished so far, the setbacks we've faced, and our hopes for the future.</p>
+                          <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Read more
+                              <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                              </svg>
+                          </a>
+                      </div>
+                  </div>
+              </div>
+          </section>
+        </div>
       </div>
 
 
