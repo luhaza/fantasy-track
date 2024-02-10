333f60d2792dfe0e84ba6e49da582bd8229cdeab
Rick Yanashita <ryanashita15@gmail.com>
2024-02-09 20:04:35 -0500

    changes to db code

--- fantasy-track/backend/config.js
+++ fantasy-track/backend/config.js
@@ -1,5 +1,7 @@
 export const PORT = 5555;
+export const PORT_meets = 3000; 
 
 export const mongoDBURL_d3ne = 'mongodb+srv://root:ieheXgnWuINXvRiw@fantasy-track.3cm5ujw.mongodb.net/test-d3ne?retryWrites=true&w=majority'
 export const mongoDBURL_users = 'mongodb+srv://root:ieheXgnWuINXvRiw@fantasy-track.3cm5ujw.mongodb.net/users?retryWrites=true&w=majority'
+export const mongoDBURL_meets = 'mongodb+srv://root:ieheXgnWuINXvRiw@fantasy-track.3cm5ujw.mongodb.net/meets?retryWrites=true&w=majority'
     // 'mongodb+srv://root:kzXsqLS11djsMZQ5@books-store-mern.0fwhnam.mongodb.net/books-collection?retryWrites=true&w=majority'
--- fantasy-track/backend/index.js
+++ fantasy-track/backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { PORT, mongoDBURL_users, mongoDBURL_d3ne } from "./config.js";
+import { PORT, mongoDBURL_users, mongoDBURL_d3ne, mongoDBURL_meets } from "./config.js";
 import mongoose from "mongoose";
+import { MongoClient } from "mongodb"; 
 import { Athlete } from "./models/athleteModel.js"
 import { User } from './models/userModel.js';
 import athletesRoute from './routes/athletesRoute.js'
@@ -12,10 +13,11 @@ import userSchema from "./models/userModel.js";
 import meetSchema from "./models/meetModel.js"; 
 
 const app = express();
+
 // Middleware for parsing request body
 app.use(express.json());
 
-//cors allow only selected
+//cors allow only selected localhost ports
 app.use(
       cors({
         origin: 'http://localhost:5173',
@@ -25,33 +27,64 @@ app.use(
 );
 
 // Middleware for handling CORS policy
-// app.use(cors());
+app.use(cors());
 
+// connecting ...
 app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send('Fantasy Track Website')
 });
 
-//athlete and meet data; 
-const conn = mongoose.createConnection(mongoDBURL_d3ne); 
-const AthleteModel = conn.model('Athlete', athleteSchema);
-const MeetModel = conn.model('Meet', meetSchema); 
-export { conn as AthleteConnection, AthleteModel}; 
-export { conn as MeetConnection, MeetModel}; 
+//MongoDB MongoClient setup for Meets Database
+const options = {};
+const dbName = 'meets'; 
+const client = new MongoClient(mongoDBURL_meets, options);
 
-//user data;
-const conn2 = mongoose.createConnection(mongoDBURL_users);
-const UserModel = conn2.model('User', userSchema); 
-export { conn2 as UserConnection, UserModel}; 
+async function connectToDatabase() {
+    try {
+      await client.connect();
+      console.log('Connected to MongoDB using MongoClient');
+  
+    //   MongoClient usage
+      const db = client.db(dbName);
+      const collections = await db.listCollections().toArray();
+
+      for (const collection of collections){
+        const collectionName = collection.name;
+        console.log(collectionName); 
+      }
+        
+      console.log('MongoClient Result');
+    } finally {
+      await client.close();
+    }
+  }
+
+connectToDatabase(); 
+
+//mongoose setup for user & d3ne database
+
+//d3ne database data
+const conn2 = mongoose.createConnection(mongoDBURL_d3ne); 
+const AthleteModel = conn2.model('Athlete', athleteSchema);
+const MeetModel = conn2.model('Meet', meetSchema); 
+export { conn2 as AthleteConnection, AthleteModel}; 
+export { conn2 as MeetConnection, MeetModel}; 
+
+//user database data;
+const conn = await mongoose.createConnection(mongoDBURL_users).asPromise();
+const UserModel = conn.model('User', userSchema); 
+export { conn as UserConnection, UserModel}; 
 
 app.use('/athletes', athletesRoute); 
 app.use('/test-users', usersRoute); 
 app.use('/meets', meetsRoute); 
 
+// Mongoose setup
 mongoose
     .connect(mongoDBURL_d3ne)
     .then(() => {
-        console.log('App connected to the database');
+        console.log('App connected to the server');
         app.listen(PORT, () => {
             console.log(`App is listening to port: ${PORT}`)
         });
@@ -60,10 +93,10 @@ mongoose
         console.log(error);
     });
 
-const db = mongoose.connection; 
+// const db = mongoose.connection; 
+const db2 = conn.useDb('users');
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
+db2.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db2.once('open', () => {
   console.log('Connected to MongoDB');
 });
-
